41eedf61b325cd88e3787f5f5984cd61fa06dec4
João Coelho <joaohg14@gmail.com>
2024-04-19 18:53:51 -0300

    Elaboração da Atividade

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+   let texto = str.replace(/ /g, '').toLowerCase();
+   let tamanho = texto.length;
+   
+   for (let i = 0; i < tamanho; i++){
+    if (texto[i] !== texto[tamanho - 1 - i]){
+        return false;
+    }
+   }
+   return true;
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let max = arr[0];
+    let min = arr[0];
+
+    for (let i = 0; i < arr.length; i++){        
+        if (arr[i] < min){
+
+            min = arr[i];
+            
+        } else if (arr[i] > max){
+
+            max = arr[i];
+        }
+    }
+
+    return [min, max];
+
 }
